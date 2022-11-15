67225d7aece295b8d310e93cee71f8b334ad2710
Nammu <namratashivagunde@gmail.com>
2022-11-15 11:47:17 -0500

    added device_map

--- main.py
+++ main.py
@@ -1,2 +1,68 @@
-import datasets from datasets
-import 
+#from  datasets import dataset
+import argparse
+import torch
+
+
+def get_arguments():
+    """Set the arguments"""
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "dataset", 
+        help="dataset name e.g. rte"
+        )
+
+    parser.add_argument(
+        "modelname",
+        help="huggingface modelname e.g. facebook/opt-125m"
+        )
+
+    parser.add_argument(
+        "--device",
+        default="cuda" if torch.cuda.is_available() else "cpu"
+        )
+
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    # get arguments
+    args = get_arguments()
+    dataset_name = args.dataset
+
+    # get dataset
+
+
+    # create prompts
+        # create a data class to load the data
+        # create a dataloader to create batches
+
+    # load model
+
+
+    # evaluate model on the validation set of the given data
+
+    
+    # compute accuracy for each data for each model
+
+
+    # save the norm attention weights of each example for each model
+
+
+    # Need a jupyter notebook which can view the norm attention weight for any validtion datapoint
+
+
+    print(dataset_name)
+    
+
+
+if __name__=='__main__':
+    main()
+
+
+
+
+
+
+
--- run-opt.py
+++ run-opt.py
@@ -3,8 +3,63 @@ import torch
 
 device = "cuda"
 
+device_map = {
+ 'model.decoder.embed_tokens': 0,
+ 'model.decoder.embed_positions': 0,
+ 'model.decoder.final_layer_norm': 1,
+ 'model.decoder.layers.0': 0,
+ 'model.decoder.layers.1': 0,
+ 'model.decoder.layers.2': 0,
+ 'model.decoder.layers.3': 0,
+ 'model.decoder.layers.4': 0,
+ 'model.decoder.layers.5': 0,
+ 'model.decoder.layers.6': 0,
+ 'model.decoder.layers.7': 0,
+ 'model.decoder.layers.8': 0,
+ 'model.decoder.layers.9': 0,
+ 'model.decoder.layers.10': 0,
+ 'model.decoder.layers.11': 0,
+ 'model.decoder.layers.12': 0,
+ 'model.decoder.layers.13': 0,
+ 'model.decoder.layers.14': 0,
+ 'model.decoder.layers.15': 0,
+ 'model.decoder.layers.16': 0,
+ 'model.decoder.layers.17': 0,
+ 'model.decoder.layers.18': 0,
+ 'model.decoder.layers.19': 0,
+ 'model.decoder.layers.20': 0,
+ 'model.decoder.layers.21': 0,
+ 'model.decoder.layers.22': 0,
+ 'model.decoder.layers.23': 0,
+ 'model.decoder.layers.24': 1,
+ 'model.decoder.layers.25': 1,
+ 'model.decoder.layers.26': 1,
+ 'model.decoder.layers.27': 1,
+ 'model.decoder.layers.28': 1,
+ 'model.decoder.layers.29': 1,
+ 'model.decoder.layers.30': 1,
+ 'model.decoder.layers.31': 1,
+ 'model.decoder.layers.32': 1,
+ 'model.decoder.layers.33': 1,
+ 'model.decoder.layers.34': 1,
+ 'model.decoder.layers.35': 1,
+ 'model.decoder.layers.36': 1,
+ 'model.decoder.layers.37': 1,
+ 'model.decoder.layers.38': 1,
+ 'model.decoder.layers.39': 1,
+ 'model.decoder.layers.40': 1,
+ 'model.decoder.layers.41': 1,
+ 'model.decoder.layers.42': 1,
+ 'model.decoder.layers.43': 1,
+ 'model.decoder.layers.44': 1,
+ 'model.decoder.layers.45': 1,
+ 'model.decoder.layers.46': 1,
+ 'model.decoder.layers.47': 1,
+ 'lm_head': 1,
+}
+
 name = "facebook/opt-30b"
-model_8bit = AutoModelForCausalLM.from_pretrained(name, device_map="auto", load_in_8bit=True)
+model_8bit = AutoModelForCausalLM.from_pretrained(name, device_map=device_map, load_in_8bit=True)
 tokenizer = AutoTokenizer.from_pretrained(name)
 
 text = "Premise === Britain said, Friday, that it has barred cleric, Omar Bakri, from returning to the country from Lebanon, where he was released by police after being detained for 24 hours \n Hypothesis === Bakri was briefly detained, but was released \n  Answer === Entailmen \n The current format presents a 'Premise', 'Hypothesis', and an 'Answer'.  How should I present this to OPT so that it is easy for OPT to answer correctly?"
