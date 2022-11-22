133312b3ec8232dc3573b8f61ec65ff0d3ae41b1
root <root@DESKTOP-947MLJD.localdomain>
2022-11-22 00:24:54 -0500

    norm attention false

--- main.py
+++ main.py
@@ -24,7 +24,8 @@ def get_arguments():
 
     parser.add_argument(
         "dataset", 
-        help="dataset name e.g. rte"
+        help="dataset name e.g. rte",
+        type=str,
         )
 
     parser.add_argument(
@@ -116,7 +117,11 @@ class NLI():
 
         # change label id to its word equivalent
         class_id_to_label = self.label_mapping()
+        print(label)
+        print(type(label))
+        print(class_id_to_label)
         label_word = class_id_to_label[int(label)]
+        print(label_word)
         
         dp = template.replace('{premise}', premise) # dp is datapoint
         dp = dp.replace('{hypothesis}', hypothesis) # filled template
@@ -130,8 +135,10 @@ class NLI():
         return dp
 
     def label_mapping(self):
+        print("hihihi")
         if self.task == 'rte':
             LM_targets = self.targets.split(';')
+            print("---------",LM_targets)
             self.class_id_to_label = {
                     0: LM_targets[0],  # entailment
                     1: LM_targets[1]}  # non-entailment
@@ -176,6 +183,8 @@ def main():
                 data_temp['demo_template'] = row['template-demo']
                 data_temp['query_template'] = row['template-query'] # this is same as demo_tempalte without the label placeholder
                 data_temp['targets'] = row['target'] # label names
+    
+    print(data_temp)
 
     # get dataset
     train_set = datasets.load_dataset('super_glue', dataset_name, split='train') # to get few shot in-context examples
@@ -246,6 +255,8 @@ def main():
             for i in range(len(batch.input_ids)):
                 label = tokenizer.decode(batch['target_input_ids'][0][1])
                 label_mapping = nli_dev_set.label_mapping()
+                print(label)
+                print(type(label))
                 label = label_mapping[int(label)]
 
                 if prob[target_words[0]][i].item() >= prob[target_words[1]][i].item(): 
