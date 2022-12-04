312309bbbd4599c5ec2a0026a18b2ab13b57cd23
root <root@DESKTOP-947MLJD.localdomain>
2022-12-04 13:04:22 -0500

    save point2

--- run-multigpu.py
+++ run-multigpu.py
@@ -13,17 +13,25 @@
 import  datasets as datasets
 import torch
 from torch.utils.data import DataLoader
-from transformers import AutoModelForCausalLM, AutoTokenizer
-from accelerate import Accelerator
+from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig
+from accelerate import Accelerator,infer_auto_device_map,init_empty_weights,load_checkpoint_and_dispatch
 
-accelerator = Accelerator();
+accelerator = Accelerator()
 
 
 def main():
     modelname= 'facebook/opt-13b'
     # device = "cuda" if torch.cuda.is_available() else "cpu"
-    device = accelerator.device
-    model = AutoModelForCausalLM.from_pretrained(modelname)
+    # device = accelerator.device
+    config = AutoConfig.from_pretrained(modelname)
+    with init_empty_weights():
+        model = AutoModelForCausalLM.from_config(config)
+    
+    device_map = infer_auto_device_map(model, no_split_module_classes=["OPTDecoderLayer"],dtype="float16")
+
+    model = AutoModelForCausalLM.from_pretrained(modelname, device_map=device_map,dtype="float16")
+    #model = load_checkpoint_and_dispatch(model, modelname, device_map=device_map)
+    
     tokenizer = AutoTokenizer.from_pretrained(modelname, return_tensors="pt")
     
     # data
